1e92521e208ed648d4f57057b3f3aad3140c9229
Sean McDonald <stmcdon19@gmail.com>
2020-09-28 19:49:41 -0400

    Added ability to re-order elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@
 
 function removeParent(ele) {
   ele.parentNode.remove();
-}
+};
+
+function moveParentUp(ele) {
+  var parent = ele.parentNode;
+  var prevParent =  parent.previousSibling;
+  parent.parentNode.insertBefore(parent, prevParent);
+};
+
+function moveParentDown(ele) {
+  var parent = ele.parentNode;
+  var nextParent =  parent.nextSibling;
+  nextParent.insertAdjacentElement("afterend",parent);
+};
 
 function submitItem(event) {
   // Set vars
@@ -11,18 +23,32 @@ function submitItem(event) {
   var li = document.createElement('li');
   var textInput = document.getElementById('inputBox').value.trim();
   var xButton = document.createElement("BUTTON");
+  var upButton = document.createElement("BUTTON");
+  var downButton = document.createElement("BUTTON");
   
   if(textInput != '') {
     // Add Text to li
     li.appendChild(document.createTextNode(textInput));
 
-    // Add Properties to Button
+    // Add Properties to xButton
     xButton.innerHTML = "X";
     xButton.id = "rmBtn";
     xButton.setAttribute('onclick', 'removeParent(this)')
+
+    // Add Properties to downButton
+    downButton.innerHTML = "move down";
+    downButton.id = "downBtn";
+    downButton.setAttribute('onclick', 'moveParentDown(this)')
+
+    // Add Properties to upButton
+    upButton.innerHTML = "move up";
+    upButton.id = "upBtn";
+    upButton.setAttribute('onclick', 'moveParentUp(this)')
     
     // Add Button to li
     li.appendChild(xButton);
+    li.appendChild(upButton);
+    li.appendChild(downButton);
 
     // Add li to ul
     ul.appendChild(li);
@@ -34,8 +60,11 @@ function submitItem(event) {
 };
 
 
+
+
 // Check for clicks on the submit button
 // addBtn.addEventListener("click", submitItem);
 
 // Check for clicks on the remove button and remove that element from the page
 // rmBtn.addEventListener("click", hello);
+
